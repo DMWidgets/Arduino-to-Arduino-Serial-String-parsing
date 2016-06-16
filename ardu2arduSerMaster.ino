c3524f7e185b1fac9da21816c1c3017b84e66e5a
Kyle Hurst <glasnoct@gmail.com>
2016-06-16 15:34:56 -0500

    Add files via upload

---
+++ ardu2arduSerMaster.ino
@@ -0,0 +1,47 @@
+#include <SoftwareSerial.h>
+const int RXpin = 10;
+const int TXpin = 9;
+SoftwareSerial myPort(RXpin, TXpin);
+
+void setup() {
+myPort.begin(9600);
+Serial.begin(9600);
+
+}
+
+void loop() {
+
+  String content = "";
+  String message = "";
+  char character;
+  char charmes;
+
+//gets string from serial monitor and readies it to send to other arduino
+  while(Serial.available()) {
+      charmes = Serial.read();
+      message.concat(charmes);
+      delay(10);
+  }
+
+//gets return message from other arduino and readies it to send to serial monitor
+  while(myPort.available()) {
+      character = myPort.read();
+      content.concat(character);
+      delay(10);
+      //content.trim();
+  }
+
+//takes readied message and sends to other arduino when there's something to send then clears payload
+if (message != "") {
+    myPort.print(message);
+    message = "";
+  }
+
+//takes readied message and sends to other serial monitor when there's something to send then clears payload
+  if (content != "") {
+    content2 = content;
+    Serial.println(content);
+    content = "";
+  }
+}
+
---
+++ ardu2arduSerSlave.ino
@@ -0,0 +1,28 @@
+#include <SoftwareSerial.h>
+const int RXpin = 9; //remember there are pin limits depending on board as to which pins can be used for Rx (pin interrupt)
+const int TXpin = 8;
+SoftwareSerial myPort(RXpin, TXpin);
+
+void setup() {
+myPort.begin(9600);
+}
+
+void loop() {
+  String content = "";
+  char character;
+
+//gets string from master and readies it to echo back to master
+  while(myPort.available()) {
+      character = myPort.read();
+      content.concat(character);
+      delay(10);
+      //content.trim();
+  }
+
+//takes readied message and sends to master when there's something to send then clears payload
+  if (content != "") {
+    myPort.print(content);
+    delay(250);
+    content = "";
+  }
+}
